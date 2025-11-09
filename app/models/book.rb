00ceccb0915b9f72ad49f7681835d6e42670dc31
ccNatang <ncole1128@gmail.com>
2025-11-08 20:49:39 -0600

    Validation adds
    Added validations to author.rb, book.rb and genre.rb

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,5 +2,5 @@ class Book < ApplicationRecord
   belongs_to :author
   belongs_to :genre
 
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: { scope: :author_id }
 end
