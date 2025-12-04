393f90b4e1ad7dfe3e5c820e29365b5389e4a438
ccNatang <ncole1128@gmail.com>
2025-12-04 00:49:43 -0600

    test: Add unit and integration tests for Books

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @book = books(:one)
+  end
+
+  test "should get index" do
+    get books_url
+    assert_response :success
+  end
+
+  test "should create book" do
+    assert_difference("Book.count") do
+      post books_url, params: { book: { title: "New Book", author_id: authors(:one).id, genre_id: genres(:one).id } }
+    end
+
+    assert_redirected_to book_url(Book.last)
+  end
 end
--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should save valid book" do
+    book = Book.new(title: "The Hobbit", author: authors(:one), genre: genres(:one))
+    assert book.save
+  end
+
+  test "should not save book without title" do
+    book = Book.new(author: authors(:one), genre: genres(:one))
+    assert_not book.save, "Saved the book without a title"
+  end
+
+  test "should not save duplicate book title per author" do
+    # Assuming 'one' exists in fixtures
+    existing_book = books(:one)
+    duplicate_book = Book.new(title: existing_book.title, author: existing_book.author, genre: existing_book.genre)
+    assert_not duplicate_book.save, "Saved duplicate book title for same author"
+  end
 end
